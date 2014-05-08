ac6cf01e83077283b1fba153eccbe5324c08b0fc
BethanyTaylor <musik258@gmail.com>
2014-05-08 16:03:20 -0600

    This is awesome! First push EVER.

--- src/lost/Game.java
+++ src/lost/Game.java
@@ -5,6 +5,7 @@
 package lost;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  *
@@ -32,6 +33,45 @@ public class Game implements Serializable{
 
     public void setTimeRemaining(double timeRemaining) {
         this.timeRemaining = timeRemaining;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" + "gameName=" + gameName + ", timeRemaining=" + timeRemaining + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 29 * hash + Objects.hashCode(this.gameName);
+        hash = 29 * hash + (int) (Double.doubleToLongBits(this.timeRemaining) ^ (Double.doubleToLongBits(this.timeRemaining) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Game other = (Game) obj;
+        if (!Objects.equals(this.gameName, other.gameName)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.timeRemaining) != Double.doubleToLongBits(other.timeRemaining)) {
+            return false;
+        }
+        return true;
+    }
+    
+    /**************************************************************************/
+    
+    
+    public static void main(String[] args){
+        System.out.println("I just want to print out something!");
+        Player playerOne = new Player();
     }
    
 }
--- src/lost/Player.java
+++ src/lost/Player.java
@@ -7,6 +7,7 @@
 package lost;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  *
@@ -54,6 +55,45 @@ public class Player implements Serializable{
 
     public void setWoodCarrying(int woodCarrying) {
         this.woodCarrying = woodCarrying;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" + "name=" + name + ", score=" + score + ", woodCamp=" + woodCamp + ", woodCarrying=" + woodCarrying + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.name);
+        hash = 29 * hash + this.score;
+        hash = 29 * hash + this.woodCamp;
+        hash = 29 * hash + this.woodCarrying;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (this.score != other.score) {
+            return false;
+        }
+        if (this.woodCamp != other.woodCamp) {
+            return false;
+        }
+        if (this.woodCarrying != other.woodCarrying) {
+            return false;
+        }
+        return true;
     }
     
     
