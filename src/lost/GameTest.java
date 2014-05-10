3ecc7265cd8a5f4829483ac0eb57070749ae051a
Olin <olinhuffman@10.50.88.172>
2014-05-10 01:11:33 -0600

    Merge origin/master
    Conflicts:
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

---
+++ src/lost/GameTest.java
@@ -0,0 +1,19 @@
+
+package lost;
+
+
+  // @author olinhuffman
+ 
+public class GameTest {
+
+  public static void main(String[] args) {
+      
+      
+      Instructions introInstructions = new Instructions();
+      introInstructions.setGameInstructions("Find wood and don't die");
+      
+      
+       
+    }
+
+}
