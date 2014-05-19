7e01c30ec946f914443b01d165c117298e4e109a
BethanyTaylor <musik258@gmail.com>
2014-05-19 16:14:00 -0600

    Added calcTime and calcPhysics function outlines

--- src/byui/cit260/LostOnAnIsland/control/ChallengeControl.java
+++ src/byui/cit260/LostOnAnIsland/control/ChallengeControl.java
@@ -6,4 +6,62 @@ package byui.cit260.LostOnAnIsland.control;
  
 public class ChallengeControl {
 
+        public double calcTime(double input, double answer){
+            
+            /*
+            BEGIN
+                GET input	
+                CHECK input
+                IF(input < 0) THEN
+                        RETURN -1
+                IF(input > 2) THEN
+                        RETURN -1
+
+                CALC time (t); distance/speed = t
+                ASSIGN t to var = correctTime
+                COMPARE correctTime to input
+                IF (correctTime == input)
+                        RETURN true
+                IF (correctTime != input)
+                        RETURN false
+
+                RETURN boolean value; var = answer
+            END
+            */
+            
+            
+            
+
+
+            return answer;
+            
+        }
+        
+        
+        public double calcPhysics(double input, double answer){
+            
+        /*    
+        BEGIN
+            GET input
+            CHECK input
+            IF (input > 0) THEN
+                    RETURN -1 (incorrect)
+            IF(input > 20) THEN
+                    RETURN -1
+
+            CALC time (t) Formula: t = (vf - vi) / a
+            ASSIGN (t) to var = correctTime
+            COMPARE correctTime to input
+            IF(correctTime == input) THEN
+                    RETURN true
+            IF(correctTime != input) THEN
+                    RETURN = false
+
+            RETURN Boolean value; var = answer
+        END
+        */
+
+            return answer;
+        }
+        
 }
