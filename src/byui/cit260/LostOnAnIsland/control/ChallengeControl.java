21038ec31dd10b0c85ce095d81dd543bdadd4e17
Olin <olinhuffman@10.50.88.172>
2014-05-21 19:14:55 -0600

    Merge origin/master
    Conflicts:
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

---
+++ src/byui/cit260/LostOnAnIsland/control/ChallengeControl.java
@@ -0,0 +1,111 @@
+
+package byui.cit260.LostOnAnIsland.control;
+
+
+  /*** @author olinhuffman*/
+ 
+public class ChallengeControl {
+
+        public static boolean calcTime(double playerGuess){
+            
+
+            
+            /*
+            BEGIN
+                GET input	
+                CHECK input
+                IF(input < 0) THEN
+                        RETURN -1
+                IF(input > 2) THEN
+                        RETURN -1
+
+                CALC minuteTime (t); distance/speed = t
+                ASSIGN t to var = correctTime
+                COMPARE correctTime to input
+                IF (correctTime == input)
+                        RETURN true
+                IF (correctTime != input)
+                        RETURN false
+
+                RETURN boolean value; var = answer
+            END
+            */
+            
+            boolean result;
+            
+            if (playerGuess < 0 || playerGuess > 2){
+                 result = false;
+            }
+            else{
+            
+                
+            
+            //Generate random numbers for distance and speed   
+            // Can not be 0
+            // Round number to one decimal place
+            
+            int speed = (int) (Math.random() * 10);
+            if (speed == 0){
+                speed = (int) (Math.random() * 10);
+            }
+                
+            System.out.println("Speed is " + speed);
+            
+            
+            int dist = (int) (Math.random() * 8);
+            if (dist == 0){
+                dist = (int) (Math.random() * 8);
+            }
+            
+            System.out.println("Distance is " + dist);
+               
+            
+            
+            //Calculate minuteTime
+            
+            double hourTime = (double)dist/(double)speed;
+            int minuteTime = (int)(hourTime * 60.0);
+            System.out.println("Time is " + minuteTime);
+            
+            if (minuteTime == playerGuess){
+                result = true;
+            }
+            else{
+                result = false;
+            }
+            
+            }
+            
+            System.out.println(result);
+            return result;
+            
+        }
+        
+        
+        public double calcPhysics(double input, double answer){
+            
+        /*    
+        BEGIN
+            GET input
+            CHECK input
+            IF (input > 0) THEN
+                    RETURN -1 (incorrect)
+            IF(input > 20) THEN
+                    RETURN -1
+
+            CALC minuteTime (t) Formula: t = (vf - vi) / a
+            ASSIGN (t) to var = correctTime
+            COMPARE correctTime to input
+            IF(correctTime == input) THEN
+                    RETURN true
+            IF(correctTime != input) THEN
+                    RETURN = false
+
+            RETURN Boolean value; var = answer
+        END
+        */
+
+            return answer;
+        }
+        
+}
---
+++ test/byui/cit260/LostOnAnIsland/control/ChallengeControlTest.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package byui.cit260.LostOnAnIsland.control;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author olinhuffman
+ */
+public class ChallengeControlTest {
+    
+    public ChallengeControlTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of calcTime method, of class ChallengeControl.
+     */
+    @Test
+    public void testCalcTime() {
+        System.out.println("calcTime");
+        double playerGuess = 0.0;
+        boolean expResult = false;
+        boolean result = ChallengeControl.calcTime(playerGuess);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of calcPhysics method, of class ChallengeControl.
+     */
+    @Test
+    public void testCalcPhysics() {
+        System.out.println("calcPhysics");
+        double input = 0.0;
+        double answer = 0.0;
+        ChallengeControl instance = new ChallengeControl();
+        double expResult = 0.0;
+        double result = instance.calcPhysics(input, answer);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
