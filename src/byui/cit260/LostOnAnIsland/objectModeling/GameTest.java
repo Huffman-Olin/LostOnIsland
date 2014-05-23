d0d1d8b0c19d45dccc26e63d6d5720614d4e1254
Olin <olinhuffman@10.50.88.172>
2014-05-23 13:19:10 -0600

    Merge origin/master
    Conflicts:
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml
    	src/byui/cit260/LostOnAnIsland/control/ChallengeControl.java

--- src/byui/cit260/LostOnAnIsland/objectModeling/GameTest.java
+++ src/byui/cit260/LostOnAnIsland/objectModeling/GameTest.java
@@ -1,16 +1,18 @@
 
 package byui.cit260.LostOnAnIsland.objectModeling;
- 
+
+
 public class GameTest {
 
   public static void main(String[] args) {
       
 
       
-    //ChallengeControl.calcTime(2);
-    //System.out.println("");
-    //ChallengeControl.printWaterNeeded();
-      
+   /* ChallengeControl.calcTime(2);
+    System.out.println("");
+    ChallengeControl.printWaterNeeded();
+    */
+      //ChallengeControlTest.testCalcTime();
       
        
 
--- test/byui/cit260/LostOnAnIsland/control/ChallengeControlTest.java
+++ test/byui/cit260/LostOnAnIsland/control/ChallengeControlTest.java
@@ -48,10 +48,11 @@ public class ChallengeControlTest {
         double distance = 3;
         double playerGuess = 120;
         boolean expResult = true;
+        
         boolean result = ChallengeControl.calcTime(playerGuess);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+       
     }
 
     /**
