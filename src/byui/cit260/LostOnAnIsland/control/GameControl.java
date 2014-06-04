ec7dd4be5771ddc32aad0fc8c9f8bda0dc3ab260
BethanyTaylor <musik258@gmail.com>
2014-06-04 16:21:39 -0600

    Added Constant class

--- src/byui/cit260/LostOnAnIsland/control/GameControl.java
+++ src/byui/cit260/LostOnAnIsland/control/GameControl.java
@@ -8,6 +8,16 @@ import byui.cit260.LostOnAnIsland.objectModeling.Player;
  
 public class GameControl {
 
+    
+    public static void startNewGame() {
+        
+        
+    }
+    
+    public static void createChallengeList() {
+        
+    }
+    
         public static Player createPlayer(String playerName) {
             System.out.println("\n**** createPlayer function called ****");
             return null;
